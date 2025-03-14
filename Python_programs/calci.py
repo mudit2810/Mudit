ee26ec266a8743a98a85d009c6461600d3e1922c
Mudit <muditjain524@gmail.com>
2025-03-14 23:35:30 +0530

    Calci prgm

---
+++ Python_programs/calci.py
@@ -0,0 +1,19 @@
+a = int(input("enter number 1 "))
+b = int(input("enter number 2 "))
+
+print("Addition of both")
+print(a+b)
+print("substraction of both")
+print(a-b)
+print("mulplication of both")
+print(a*b)
+print("division of both")
+print(a/b)
+
+print("the value of :",a ,"+" ,b , "is",a+b)
+print("the value of :",a ,"-" ,b , "is",a-b)
+print("the value of :",a ,"*" ,b , "is",a*b)
+print("the value of :",a ,"/" ,b , "is",a/b)
+
+
+
