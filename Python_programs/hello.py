a7e1efa4a3ce85187a54ed9de3e590b7be272872
Mudit <muditjain524@gmail.com>
2025-03-14 22:51:39 +0530

    Added escp seq

--- Python_programs/hello.py
+++ Python_programs/hello.py
@@ -1,3 +1,29 @@
+'''
+hello how are you
+I am fine
+'''
 print("hello world")
 print(5)
 print("bye bye")
+
+# escape character sequence
+
+print("Hi  i am mudit \nI am working in cognizant")
+
+# if want to add charac in string like ''
+
+print("Hello my friend \"Rahul\"")
+
+# if we want to multiple print in one line
+
+print("hey",6,1)
+
+# if we want to sep this by default sep is space
+
+print("hey",6,1 ,sep='~')
+
+#if we want end with something by default end is new line
+
+print("hey",6,1 , end='')
+print("Hare krishna")
+
