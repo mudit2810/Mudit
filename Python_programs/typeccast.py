8ea8f2d0df33049f2a385576f4e6be2a7c1c2238
Mudit <muditjain524@gmail.com>
2025-03-15 12:18:18 +0530

    Typecast prgm

---
+++ Python_programs/typeccast.py
@@ -0,0 +1,18 @@
+# typecasting to convert data type into other data type is know as type casting 
+
+a = '1'
+b = '2'
+print(a+b)
+
+# if we want to typecast this
+print(int(a)+int(b))
+
+# there two type of typecasting
+# explict - done by the programmer explicity
+# implicit - done by the python it self to convert lower DT to Higher DT automatically
+c = 9.6
+print(type(c))
+d = 2
+print(type(d))
+e = c+d
+print(e,type(e))
