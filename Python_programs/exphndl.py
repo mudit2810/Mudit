c2c87628cef86a9df50c9e90015e12d794763a11
Mudit <muditjain524@gmail.com>
2025-03-22 11:50:05 +0530

    new progrm

--- Python_programs/exphndl.py
+++ Python_programs/exphndl.py
@@ -20,3 +20,30 @@ except ValueError:
     print("Not a number")
 except IndexError:
     print("Not an vaild index")
+
+# https://career5.successfactors.eu/careers?company=capgemitecP3
+
+## finally keyword: I am always executed whatever the condition
+
+# The finally code block is also a part of exception handling.When we handle exception using 
+# the try and except block, we can include a finally block at the end.
+# The finally block is always executed, so it is generally used for doing the concluding tasks like closing file resources 
+# or closing database connection or may be ending the program execution with a delightful message.
+
+def func1():
+  try:
+    l = [1, 5, 6, 7]
+    i = int(input("Enter the index: "))
+    print(l[i])
+    return 1
+  except:
+    print("Some error occurred")
+    return 0
+
+  finally:
+    print("I am always executed")
+  # print("I am always executed")
+
+
+x = func1()
+print(x)
