a06597742ef66bf908e8434ec2813857670b0a93
Mudit <muditjain524@gmail.com>
2025-03-23 12:40:54 +0530

    Added new profram file

---
+++ Python_programs/shorthandif.py
@@ -0,0 +1,7 @@
+a = 300
+b = 60
+
+print("A") if a > b else print('=') if a == b else print('B')
+
+#There is also a shorthand syntax for the if-else statement that can be
+#  used when the condition being tested is simple and the code blocks to be executed are short.
